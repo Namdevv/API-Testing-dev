16db55d947479eba1fd7228d83bc8b4ecb54c766
truong51972 <tranquoctruong20@gmail.com>
2025-08-22 20:56:14 +0700

    add langchain dev

--- agent-service/src/graph/workflows/.langgraph_dev.py
+++ agent-service/src/graph/workflows/.langgraph_dev.py
@@ -1,9 +1,15 @@
-from src.graph.workflows.docs_preprocessing import DocsPreprocessingWorkflow
+from src.registry.actions import scan_and_register_actions
 from src.registry.nodes import scan_and_register_nodes
+from src.registry.tools import scan_and_register_tools
+from src.registry.workflows import WORKFLOW_REGISTRY, scan_and_register_workflows
 
+scan_and_register_tools()
+scan_and_register_actions()
 scan_and_register_nodes()
-docs_preprocessing_workflow = DocsPreprocessingWorkflow(
-    collection_name="test",
-    llm_temperature=0.1,
-)
+scan_and_register_workflows()
+
+docs_preprocessing_workflow = WORKFLOW_REGISTRY.get("docs_preprocessing")()
 docs_preprocessing_graph = docs_preprocessing_workflow.get_graph()
+
+simple_qa_workflow = WORKFLOW_REGISTRY.get("simple_qa")()
+simple_qa_graph = simple_qa_workflow.get_graph()
