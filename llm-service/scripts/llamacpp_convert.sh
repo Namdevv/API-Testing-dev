aea21c89234a61cfa9ba171dad3ae17f32cc4cd1
truong51972 <tranquoctruong20@gmail.com>
2025-09-05 20:06:14 +0700

    feat: convert and quantize script

---
+++ llm-service/scripts/llamacpp_convert.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+CURRENT_PATH=$(pwd)
+
+# Get the model name from the first argument
+MODEL_NAME="$1"
+shift
+
+VOLUME_MOUNT="$CURRENT_PATH/models/"
+OUT_TYPE="f16"
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --volume-mount)
+            VOLUME_MOUNT="$2"
+            shift # skip next value since it's been used
+            ;;
+        --out-type)
+            OUT_TYPE="$2"
+            shift
+            ;;
+        *)
+            echo "Unknown option: $key"
+            exit 1
+            ;;
+    esac
+    shift # skip current tag
+done
+
+if [ -d "$VOLUME_MOUNT" ]; then
+    echo "'$VOLUME_MOUNT' exists!"
+else
+    echo "'$VOLUME_MOUNT' does not exist!"
+    echo "Checking parent directory..."
+    PARENT_PATH=$(dirname "$CURRENT_PATH")
+    VOLUME_MOUNT="$PARENT_PATH/models/"
+
+    if [ -d "$VOLUME_MOUNT" ]; then
+        echo "'$VOLUME_MOUNT' exists!"
+    else
+        echo "'$VOLUME_MOUNT' does not exist!"
+        exit 1
+    fi
+fi
+
+echo
+echo "---------------------------------------------------------------------"
+echo
+
+docker run --rm -v $VOLUME_MOUNT:/models \
+    ghcr.io/ggml-org/llama.cpp:full \
+    --convert /models/$MODEL_NAME \
+    --outfile /models/$MODEL_NAME/model.gguf \
+    --outtype $OUT_TYPE \
