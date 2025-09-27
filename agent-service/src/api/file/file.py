9445606e88e9e720217aee676f95fce166d5f4d8
truong51972 <tranquoctruong20@gmail.com>
2025-09-27 12:03:45 +0700

    update response

--- agent-service/src/api/file/file.py
+++ agent-service/src/api/file/file.py
@@ -1,14 +1,20 @@
 from uuid import uuid4
 
 from fastapi import APIRouter, File, UploadFile
+from pydantic import BaseModel
 
 from src.settings import RUNTIME_DATA_DIR
 
 router = APIRouter(prefix="/file", tags=["File"])
 
 
+class FileResponseModel(BaseModel):
+    file_id: str
+    file_name: str
+
+
 @router.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(file: UploadFile = File(...)) -> FileResponseModel:
     contents = await file.read()
 
     file_id = str(uuid4()) + "." + file.filename.split(".")[-1]
@@ -17,4 +23,4 @@ async def upload_file(file: UploadFile = File(...)):
     with open(file_path, "wb") as f:
         f.write(contents)
 
-    return {"file_id": file_id}
+    return FileResponseModel(file_id=file_id, file_name=file.filename)
