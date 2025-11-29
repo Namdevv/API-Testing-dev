3b16a6f6ac104a28c3ec926cc1d634a9069c2380
truong51972 <tranquoctruong20@gmail.com>
2025-11-29 12:18:10 +0700

    add new api

--- agent-service/src/api/routers.py
+++ agent-service/src/api/routers.py
@@ -1,19 +1,17 @@
-from src.api.agent.agent_api import router as agent_router
-from src.api.common.common_api import router as common_router
-from src.api.document.document_api import router as document_router
-from src.api.file.file import router as router_file
-from src.api.project.project import router as project_router
-from src.api.test_suite.test_suite_api import router as test_suite_router
-from src.api.test_cases.test_case_generation_api import (
-    router as test_case_generation_router,
+from .agent.agent_api import router as agent_router
+from .common.common_api import router as common_router
+from .document.document_api import router as document_router
+from .file.file import router as router_file
+from .project.project import router as project_router
+from .test_entities.test_entities_api import (
+    router as test_entities_router,
 )
 
 all_routers = [
     common_router,
     agent_router,
-    test_suite_router,
     router_file,
     document_router,
     project_router,
-    test_case_generation_router,
+    test_entities_router,
 ]
