b8e861614ea7dc76e2750dca25288069aa5bc6ea
truong51972 <tranquoctruong20@gmail.com>
2025-12-17 21:50:09 +0700

    update for deployment

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,13 @@
+cd agent-service
+./scripts/build_image.sh --forge_build
+cd ..
+
+cd frontend
+./deploy/scripts/run.sh build
+cd ..
+
+./gateway-service/deploy/scripts/run.sh
 ./agent-service/deploy/scripts/run.sh
 ./document-service/deploy/scripts/run.sh
-./gateway-service/deploy/scripts/run.sh
-./llm-service/deploy/scripts/run.sh
+# ./llm-service/deploy/scripts/run.sh
 ./frontend/deploy/scripts/run.sh
